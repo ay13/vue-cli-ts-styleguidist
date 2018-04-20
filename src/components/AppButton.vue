173752745db3f22758e5c9f11dea3f495e391656
Andrew Young <aycodes@gmail.com>
2018-04-20 12:36:27 -0500

    adding styleguidist and a component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,80 @@
+<template>
+  <button :style="styles" @click="handleClick">
+    <!-- @slot Use this slot to place the button content -->
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'app-button',
+  props: {
+    /** Sets the button font color
+     * @deprecated Use color instead
+     */
+    color: {
+      type: String,
+      default: 'black',
+    },
+    /** Sets background color of the button
+     * @since 1.2.0
+     */
+    background: {
+      type: String,
+      default: 'white',
+    },
+  },
+  computed: {
+    styles() {
+      return {
+        color: this.color,
+        background: this.background,
+      };
+    },
+  },
+  methods: {
+    /** Triggered when button is clicked
+     * @event click
+     * @type {Event}
+     */
+
+    /** Event for Alligator's example
+     * @event gator
+     * @type {Event}
+     */
+    handleClick(e) {
+      this.$emit('click', e);
+    },
+  },
+});
+</script>
+
+<docs>
+This button is amazing, use it responsibly.
+
+## Examples
+
+Orange button:
+
+```jsx
+<app-button color="orange">Push Me</app-button>
+```
+
+Ugly button with pink font and blue background:
+
+```jsx
+<app-button color="pink" background="blue">
+  Ugly button
+</app-button>
+```
+
+Button containing custom tags:
+
+```jsx
+<app-button>
+  Text with <b>bold</b>
+</app-button>
+```
+</docs>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <app-button color="orange">This is a button</app-button>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import AppButton from '@/components/AppButton.vue';
 
 export default Vue.extend({
   name: 'home',
   components: {
     HelloWorld,
+    AppButton,
   },
 });
 </script>
