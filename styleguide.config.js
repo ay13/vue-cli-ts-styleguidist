89c71380028c775f565f646ae7f59106cc8af24e
Unknown <aycodes@gmail.com>
2018-04-23 13:46:19 -0500

    Updating styleguidist config to use vue webpack config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,19 +1,7 @@
-const { resolve } = require('path');
-const rules = require('vue-webpack-loaders');
-
 module.exports = {
   components: './src/components/**/[A-Z]*.vue',
   webpackConfig: {
-    resolve: {
-      extensions: ['.js', '.json', '.vue', '.ts'],
-      alias: {
-        '~': resolve(__dirname, 'src'),
-        '@': resolve(__dirname, 'src')
-      }
-    },
-    module: {
-      rules
-    }
+    module: require("./node_modules/@vue/cli-service/webpack.config.js").module
   },
   showUsage: true,
   showCode: true
